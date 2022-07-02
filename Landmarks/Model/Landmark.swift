a3b4b9284ca00173e2ae7111ec8d99fecb13d37f
mitras-git <myemail.aditya@gmail.com>
2022-07-03 01:04:18 +0300

    Added a bunch of stuff

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,35 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by मित्रा on 03/07/2022.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Codable, Hashable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )}
+    
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
---
+++ Landmarks/View/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by मित्रा on 03/07/2022.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
